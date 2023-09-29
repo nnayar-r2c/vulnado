38856e6f6093b56e17c09550af5a3db03ccbbe5a
Nitin Nayar <nitin.nayar@gmail.com>
2023-09-29 08:49:54 +0100

    some very new vulnerable updates

---
+++ vulns/very-new-vul.java
@@ -0,0 +1,65 @@
+public class ActiveDebugCode{
+
+    public void bad(){
+        StackTraceElement[] elements;
+
+        Exception e = new Exception();
+        elements = e.getStackTrace();
+
+        // ruleid: active-debug-code-getstacktrace
+        System.err.print(elements);
+    }
+
+    public void bad2(){
+        StackTraceElement[] elements;
+
+        elements = Thread.currentThread().getStackTrace();
+
+        // ruleid: active-debug-code-getstacktrace
+        System.err.print(elements);
+    }
+
+    public void bad3(){
+        StackTraceElement[] elements;
+
+        elements = new Throwable().getStackTrace();
+
+        // ruleid: active-debug-code-getstacktrace
+        System.err.print(elements);
+    }
+
+    public void bad4(){
+        // ruleid: active-debug-code-getstacktrace
+        System.out.println(org.apache.commons.lang3.exception.ExceptionUtils.getStackTrace(e));
+        // ruleid: active-debug-code-getstacktrace
+        System.out.println(org.apache.commons.lang3.exception.ExceptionUtils.getFullStackTrace(e));
+    }
+
+    public void alsobad(){
+        for (StackTraceElement ste : Thread.currentThread().getStackTrace()) {
+            // ruleid: active-debug-code-getstacktrace
+            System.out.println(ste);
+        }
+    }
+    public void alsoverybad(){
+        for (StackTraceElement ste : Thread.currentThread().getStackTrace()) {
+            // ruleid: active-debug-code-getstacktrace
+            System.out.println(ste);
+        }
+    }
+
+    public void alsoveryverybad(){
+        for (StackTraceElement ste : Thread.currentThread().getStackTrace()) {
+            // ruleid: active-debug-code-getstacktrace
+            System.out.println(ste);
+        }
+    }
+
+    public void alsoveryverybad(){
+        for (StackTraceElement ste : Thread.currentThread().getStackTrace()) {
+            // ruleid: active-debug-code-getstacktrace
+            System.out.println(ste);
+        }
+    }
+
+}
